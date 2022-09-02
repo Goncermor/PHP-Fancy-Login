e07c415c4424b1623dbf7b5b4c1d0b36a0d365e4
Gonçalo Certo Morais <goncalocerto9morais@gmail.com>
2022-09-02 11:36:12 +0100

    Added new images and cycling trough them

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const Sleep = m => new Promise(r => setTimeout(r, m));
 
-async function loaded() {
-    await Sleep(30000);
-    document.body.style.backgroundImage = 'url("img/2.webp")';
+function loaded() {
+    backgroundLoop();   
+}
+
+async function backgroundLoop() {
+    while (true) {
+        for (let i = 1; i < 4; i++) {
+            await Sleep(3000);
+            console.log(i);
+            document.body.style.backgroundImage = 'url("img/' + i + '.webp")';
+        }
+    }
 }
