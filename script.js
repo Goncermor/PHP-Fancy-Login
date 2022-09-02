532d5b46909f61a8dba0be671203035b6394149d
Gonçalo Certo Morais <goncalocerto9morais@gmail.com>
2022-09-02 11:02:32 +0100

    Changed image update time to 30 seconds

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const Sleep = m => new Promise(r => setTimeout(r, m));
 
 async function loaded() {
-    await Sleep(2000);
+    await Sleep(30000);
     document.body.style.backgroundImage = 'url("img/2.webp")';
 }
