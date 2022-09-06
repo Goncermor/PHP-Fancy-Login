d2902114e81e1d10514d9b6b34a0d25e41184f71
Gonçalo Morais <goncalocerto9morais@gmail.com>
2022-09-07 00:51:59 +0100

    Changed by text Added php backend Finished the script to work with the backend

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+$encryption_iv = '0744641209477128';
+$encryption_key = "Goncermor-Fancy-Login";
+if (isset($_COOKIE['data'])) {
+    $cookieJson = json_decode(openssl_decrypt($_COOKIE['data'], "AES-128-CTR", $encryption_key, 0, $encryption_iv));
+if (ProcessInfo($cookieJson->user, $cookieJson->password) === true) {
+  http_response_code(201);
+  header("Location: https://google.com/");
+  die;
+} else {
+  http_response_code(401);
+  unset($_COOKIE['data']); 
+  setcookie('data', null, -1, '/'); 
+}}
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+  $input = file_get_contents("php://input");
+  $json = json_decode($input);
+  $status = ProcessInfo($json->username, $json->password);
+  if ($status === true) {
+    http_response_code(201);
+    $encryption = openssl_encrypt($input, "AES-128-CTR", $encryption_key, 0, $encryption_iv);
+    echo json_encode(array('status' => true, 'data' => $encryption));
+  } else {
+    http_response_code(401);
+    echo json_encode(array('status' => false));
+  }
+  die;
+}
+function ProcessInfo($user, $password) {
+  return true;
+  // place here the code to check if the user exists
+  // return true to save the login and continue
+  // return false to send invalid password
+}
+?>
--- script.js
+++ script.js
@@ -1,48 +1,41 @@
 const Sleep = m => new Promise(r => setTimeout(r, m));
-
 let InputError = false;
-
 async function send() {
   let usernameInput = document.getElementById('username');
   let passwordInput = document.getElementById('password');
-    if (usernameInput.value.length <= 8) {
-        InputError = true;
-        usernameInput.style.borderColor = "#ff0000da";
-        console.log("Username is not valid.");
-    } else {console.log("Username is valid.");}
-    if (passwordInput.value.length <= 8) {
-        InputError = true;
-        passwordInput.style.borderColor = "#ff0000da";
-        console.log("Password is valid.");
-    }
-    if (InputError == true) {
-        await Sleep(2000);
-        InputError = false;
-        usernameInput.style.borderColor = "#ffffff6a";
-        passwordInput.style.borderColor = "#ffffff6a";
-        return;
-    } else {console.log("Password is not valid.");}
-    grecaptcha.ready(function() {
-      console.log("reCaptcha ready!");
-      grecaptcha.execute('6LfUHNQhAAAAAO4bjLnPtltN6EQZEgL9rPq3dSQm').then(function(token) {
-        console.log("reCaptcha executing...");
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://demo.goncermor.com:5555/", false);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        console.log("Request opened...");
-        let data = JSON.stringify({
-          username: usernameInput.value,
-          password: passwordInput.value,
-          token: token
-        });
-        xhr.send(data);
-        console.log("Sending: e" + data);
-        console.log("Request sent.");
-
-
-
-
-
-      });
-    });
+  if (usernameInput.value.length <= 8) {
+      InputError = true;
+      usernameInput.style.borderColor = "#ff0000da";
+      console.log("Username is not valid.");
+  } else {console.log("Username is valid.");}
+  if (passwordInput.value.length <= 8) {
+      InputError = true;
+      passwordInput.style.borderColor = "#ff0000da";
+      console.log("Password is valid.");
+  }
+  if (InputError == true) {
+    InputError = false;
+    await Sleep(2000);
+    usernameInput.style.borderColor = "#ffffff6a";
+    passwordInput.style.borderColor = "#ffffff6a";
+    return;
+  } else {console.log("Password is not valid.");}
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://demo.goncermor.com/", false);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  console.log("Request opened...");
+  let data = JSON.stringify({
+    username: usernameInput.value,
+    password: passwordInput.value
+  });
+  console.log("Sending: " + data);
+  xhr.send(data);
+  console.log("Request sent.");
+  let cookiedata = JSON.parse(xhr.response);
+  console.log("Received and parsed data: " + cookiedata.data);
+  const d = new Date();
+  d.setTime(d.getTime() + (31*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = "data=" + cookiedata.data + ";" + expires + ";path=/";
+  window.location.reload();
 }
